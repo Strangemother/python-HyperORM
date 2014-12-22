ddf3a3990d7f98e2ec089f00ba7ac73e4def1375
Jay Jagpal <jay@strangemother.com>
2014-12-22 14:57:49 -0500

    wip file prior to project name change

---
+++ api/wip.py
@@ -0,0 +1,16 @@
+from examples.phonebook import phonebook
+from examples.phonebook import phonebook_put_many
+space = phonebook()
+M = space.get_model()
+m = M('tom', first='Tomas', last='Thumb', phone=440743566523)
+
+m=space.get('bob')
+m.get_attrs()
+m.last
+m.last = 'foo'
+m.save()
+m.get()
+phonebook_put_many()
+
+m=pb.get_model()()
+m.get_def()
