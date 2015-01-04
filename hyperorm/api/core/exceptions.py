780a6d49e2756cf8715afb746e758a6418b85377
Jay Jagpal <jay@strangemother.com>
2015-01-04 22:33:02 +0000

    Additional errors for hyperorm

--- hyperorm/api/core/exceptions.py
+++ hyperorm/api/core/exceptions.py
@@ -3,7 +3,9 @@ from collections import namedtuple
 
 ErrorDefinitions = {
     'MISSING_KEY': 'Key field is not defined in model "%(model.__class__.__name__)s"',
-    'ADMIN_ACCESS_DENIED': 'This task required admin privileges'
+    'ADMIN_ACCESS_DENIED': 'This task required admin privileges',
+    'MISSING_SERVICE': 'The Service is required',
+    'HYPERDEX_CLIENT_COORDFAIL': 'Client Coordination failure'
 }
 
 
@@ -16,17 +18,20 @@ ERR = Struct(**ErrorDefinitions)
 
 
 class HyperError(Exception):
+    code = -1
+    message = 'Error'
+    model = None
 
-    def __init__(self, code, message=None, model=None):
-        self.code = code
-        self.message = message
-        self.model = None
+    def __init__(self, code=None, message=None, model=None):
+        self.code = code if code is not None else self.__class__.code
+        self.message = message  if message is not None else self.__class__.message
+        self.model = model if model is not None else self.__class__.model
 
     def __str__(self):
 
         name = self.__class__.__name__
 
-        c = self.code % {'model': self.model,
+        c = self.code % {'model': self.model or {},
                          'code': self.code,
                          'message': self.message
                          }
@@ -37,3 +42,15 @@ class HyperError(Exception):
         else:
             s = '%s: %s' % (name, self.code)
             return repr(s)
+
+
+class MissingServiceError(HyperError):
+    message = 'Service is None or not provided'
+
+
+class HyperClientError(HyperError):
+    pass
+
+class MissingClientError(HyperClientError):
+    message = 'A service Client is None or not provided'
+
